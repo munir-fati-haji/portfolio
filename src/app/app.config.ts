de19eb418ffdad3a7bfd0d678cd4481a777d9daf
Munir Fati Haji <munir.fati.haji@gmail.com>
2026-06-28 01:45:41 +0200

    Provide HTTP client for SVG icons

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -7,6 +8,7 @@ import { provideHighlightOptions } from 'ngx-highlightjs';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideHttpClient(),
     provideRouter(routes),
     provideHighlightOptions({
       coreLibraryLoader: () => import('highlight.js/lib/core'),
