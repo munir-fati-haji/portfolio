80f71b34904262bfc4ae384884d090f6f0742af7
Munir Fati Haji <munir.fati.haji@gmail.com>
2026-07-07 17:48:55 +0200

    scss

--- src/app/core/components/portfolio/components/footer/footer.ts
+++ src/app/core/components/portfolio/components/footer/footer.ts
@@ -1,20 +1,17 @@
-import { Component, inject } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
+import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { ThemeService } from '@core/services/theme/theme.service';
 import { FOOTER_COPYRIGHT } from './data/footer-copyright';
 import { FOOTER_LINKS } from './data/footer-links';
 
 @Component({
   selector: 'app-footer',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatTooltipModule],
+  imports: [MatToolbarModule, MatIconModule, MatTooltipModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
 export class Footer {
-  protected readonly themeService = inject(ThemeService);
   protected readonly copyright = FOOTER_COPYRIGHT;
   protected readonly links = FOOTER_LINKS;
 }
